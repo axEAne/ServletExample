31a7e0de7e62bbd5ecda635eb290a11e4d0b1836
Alex Kononchuk <axeane@gmail.com>
2019-03-28 21:47:32 +0100

    Added sql script for creating and filling the database.

---
+++ script.sql
@@ -0,0 +1,19 @@
+#sql_data - name of your database. change it.
+USE sql_data; 
+ 
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+id INT NOT NULL AUTO_INCREMENT, 
+name VARCHAR(45) NOT NULL, 
+age INT NOT NULL
+, PRIMARY KEY (id)
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+ 
+INSERT INTO sql_data.users
+(name, age)
+VALUES 
+('Alex', '28'),
+('Ann', '26')
